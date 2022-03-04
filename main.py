35789426573b31c228bc8504c35c02d4dd8720e1
niels <nielsvandenbroeck11@gmail.com>
2022-03-04 16:12:06 +0100

    CST -> AST

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from antlr4 import *
 from grammar1Lexer import grammar1Lexer
 from grammar1Parser import grammar1Parser
+from antlr4.tree.Trees import Trees
 
 
 def main():
@@ -10,9 +11,21 @@ def main():
     stream = CommonTokenStream(lexer)
     parser = grammar1Parser(stream)
     tree = parser.program()
+    print(dir(tree))
+    print(tree.getText())
+    printTree(tree)
+    print(tree.getText())
+
+def printTree(tree):
+    if(tree.getText() == '(' or tree.getText() == ')'):
+        return True
+    if(tree.getChildCount() == 0):
+        return False
+    for child in tree.getChildren():
+        if(printTree(child)):
+            tree.removeLastChild()
+    return False
 
 
 if __name__ == '__main__':
     main()
-    print(+++5)
-    print("test");
